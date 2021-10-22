364df328be0add3f50d7f04852262c7cc1e885e8
campionfellin <campionfellin@gmail.com>
2021-10-21 20:11:56 -0700

    feat: Deploy website
    Signed-off-by: campionfellin <campionfellin@gmail.com>

---
+++ infra/bin/infra.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App } from '@aws-cdk/core';
+import { ManifoldAwsFrontendStackLib } from '@manifoldxyz/manifold-aws-frontend-stack-lib'
+
+const app = new App();
+new ManifoldAwsFrontendStackLib(app, 'RoyaltyRegistryStack', {
+  websiteName: 'RoyaltyRegistry',
+  domainName: 'royaltyregistry.xyz',
+  env: {
+    account: '743799374440',
+    region: 'us-east-1'
+  }
+})
