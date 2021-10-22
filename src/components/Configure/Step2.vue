d8ddf306653f953b48ad744b6c3aab04c7218ebb
Wilkins Chung <wilkins.chung@gmail.com>
2021-10-21 22:44:47 -0700

    change text for contracts recognized as already having on-chain royalties

--- src/components/Configure/Step2.vue
+++ src/components/Configure/Step2.vue
@@ -4,8 +4,8 @@
       <span class="step-label">Step 2 / 4 : Token Royalties</span>
       <template v-if="!!royaltySpec">
         <h2>Token Contract Royalty Spec</h2>
-        <span class="text">This Token already supports <strong>{{ royaltySpec }}</strong>.</span>
-        <span v-if="royaltySpec == 'Manifold'" class="text">You should configure your royalties using <a href="https://studio.manifoldxyz.dev">Manifold Studio</a> instead of creating a new override.</span>
+        <span class="text">This Token already supports <strong>{{ royaltySpec }}</strong> and you <strong>DO NOT</strong> need to set a new override.</span>
+        <span v-if="royaltySpec == 'Manifold'" class="text">You should configure your royalties using <a href="https://studio.manifoldxyz.dev">Manifold Studio</a>.</span>
       </template>
       <template v-if="!!overrideAddress">
         <h2>Active Overrides</h2>
